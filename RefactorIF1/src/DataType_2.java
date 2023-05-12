a68cf2f080c5ef43bc1470f89f3865bddf0728c1
Ferran <fmontblanch@gmail.com>
2023-05-12 10:45:40 +0200

    Segona Refactorizació

--- RefactorIF1/src/DataType_2.java
+++ RefactorIF1/src/DataType_2.java
@@ -7,6 +7,7 @@ public class DataType_2 {
         System.out.println(DataType.gd(0));
     }
 
+
     public static String gd(int code) {
         int[] dateCodes = {702, 1082, 1083, 1114, 1184, 1266, 12403};
         int[] numericCodes = {20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
